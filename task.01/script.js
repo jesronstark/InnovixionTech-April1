8252c861eba2487aafb71d42496e794c911ea6a1
jesronstark <162868061+jesronstark@users.noreply.github.com>
2024-04-19 11:09:57 +0530

    Add files via upload

---
+++ task.01/script.js
@@ -0,0 +1,16 @@
+const countdownDate = new Date('2024-12-31T00:00:00').getTime();
+    const timer = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  if (distance < 0)
+   {
+    clearInterval(timer);
+    document.getElementById('countdown').innerHTML = 'EXPIRED';
+   }
+}, 1000);
